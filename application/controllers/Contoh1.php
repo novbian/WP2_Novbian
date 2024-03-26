cda682a092857d52397c21effcb82b892e1e3b21
novbian <novbianbramana10@gmail.com>
2024-03-26 19:24:09 +0700

    Tugas pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contoh1 extends \CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<p>Nama saya Novbian.</p>";
+        echo "<p>Saya tinggal di daerah Setu.</p>";
+    }
+}
