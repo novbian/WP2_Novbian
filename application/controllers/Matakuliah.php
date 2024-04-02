d1361239d1f3a35ed2f53a5b9f5b56873658d598
novbian <novbianbramana10@gmail.com>
2024-04-02 14:34:23 +0700

    Tugas Pertemuan 04

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -3,19 +3,51 @@ class Matakuliah extends CI_Controller
 {
     public function index()
     {
-        // $this->load->helper('url');
+        // $this->load->helper(array('form', 'url'));
+        // $this->load->library('form_validation');
         $this->load->view('view-form-matakuliah');
     }
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
+        $this->load->library('form_validation');
+        if ($this->input->method() === 'post') {
+
+            $this->form_validation->set_rules(
+                'kode',
+                'Kode Matakuliah',
+                'required|min_length[3]',
+                [
+                    'required' => 'Kode Matakuliah harus diisi',
+                    'min_length' => 'Kode terlalu pendek'
+                ]
+            );
+
+            $this->form_validation->set_rules(
+                'nama',
+                'Nama Matakuliah',
+                'required|min_length[3]',
+                [
+                    'required' => 'Nama Matakuliah harus diisi',
+                    'min_length' => 'Nama terlalu pendek'
+                ]
+            );
+
+            if ($this->form_validation->run() == false) {
+                return $this->load->view('view-form-matakuliah');
+            } else {
+                $data = [
+                    'kode' => $this->input->post('kode'),
+                    'nama' => $this->input->post('nama'),
+                    'sks' => $this->input->post('sks')
+                ];
+                $this->load->view('view-data-matakuliah', $data);
+            }
+        }
+
+
+
 
         // $this->load->helper('url');
-        $this->load->view('view-data-matakuliah', $data);
     }
 }
